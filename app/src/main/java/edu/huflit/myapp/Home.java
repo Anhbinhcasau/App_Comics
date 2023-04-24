7803d8e23118c74022a38cf1c006877d3841f924
Anhbinhcasau <huynhthanhbinhbl2003@gmail.com>
2023-04-24 13:12:38 +0700

    sync

--- app/src/main/java/edu/huflit/myapp/Home.java
+++ app/src/main/java/edu/huflit/myapp/Home.java
@@ -1,6 +1,5 @@
 package edu.huflit.myapp;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -58,9 +57,9 @@ public class Home extends AppCompatActivity {
 
     private Timer mTimer;
 
-    EditText edTSearch,edtName,edtEmail;
+    EditText edTSearch, edtName, edtEmail;
 
-    String email,tentaikhoan;
+    String email, tentaikhoan;
 
     ListView listviewthongtin, listviewmanhinhchinh;
 
@@ -82,7 +81,7 @@ public class Home extends AppCompatActivity {
 
         //Lấy dữ liệu từ trang Login qua
         Intent intent = getIntent();
-        int pk = intent.getIntExtra("phanquyen",0);
+        int pk = intent.getIntExtra("phanquyen", 0);
         email = intent.getStringExtra("Email");
         tentaikhoan = intent.getStringExtra("TaiKhoan");
 
@@ -93,7 +92,7 @@ public class Home extends AppCompatActivity {
         ActionBar();
         //Phương thức lấy ảnh để set lên viewpager
         mListPhoto = GetListPhoto();
-        photoAdater = new PhotoAdater(this,mListPhoto);
+        photoAdater = new PhotoAdater(this, mListPhoto);
         viewPager.setAdapter(photoAdater);
         //Hàm animation
         autoImage();
@@ -110,17 +109,16 @@ public class Home extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 //Thông Tin Cá Nhân
-                if(i == 0) {
+                if (i == 0) {
                     Intent intent = new Intent(Home.this, Layout_User.class);
                     startActivity(intent);
                 }
                 //Đăng bài
                 else if (i == 1) {
-                    if(pk == 1) {
+                    if (pk == 1) {
                         Intent intent1 = new Intent(Home.this, LayoutAdmin.class);
                         startActivity(intent1);
-                    }
-                    else{
+                    } else {
                         Toast.makeText(Home.this, "Bạn không có quyền đăng bài", Toast.LENGTH_SHORT).show();
                     }
                 }
@@ -136,7 +134,7 @@ public class Home extends AppCompatActivity {
                 }
                 //Đổi Mật Khẩu
                 else if (i == 4) {
-                    Intent intent = new Intent(Home.this,ChangePass.class);
+                    Intent intent = new Intent(Home.this, ChangePass.class);
                     startActivity(intent);
                 }
                 //Setting
@@ -185,10 +183,10 @@ public class Home extends AppCompatActivity {
         Cursor cursor = dtbapp.getDataTruyen();
         tranhArrayList = new ArrayList<TruyenTranh>();
 
-        while (cursor.moveToNext()){
-            TruyenTranh truyenTranh= new TruyenTranh();
+        while (cursor.moveToNext()) {
+            TruyenTranh truyenTranh = new TruyenTranh();
             int id = cursor.getInt(0);
-            String Ten =cursor.getString(1);
+            String Ten = cursor.getString(1);
             String anh = cursor.getString(3);
             truyenTranh.setIdTruyen(id);
             truyenTranh.setTenTruyen(Ten);
@@ -198,9 +196,10 @@ public class Home extends AppCompatActivity {
         cursor.moveToFirst();
         //Thực hiện khi không sử dụng
         cursor.close();
-        adapter = new TruyenTranhAdapter(this,0,tranhArrayList);
+        adapter = new TruyenTranhAdapter(this, 0, tranhArrayList);
         gridView.setAdapter(adapter);
     }
+
     private void SetUp() {
         gridView.setAdapter(adapter);
     }
@@ -225,28 +224,27 @@ public class Home extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawerlayout);
         //Thông tin
         usersArrayList = new ArrayList<>();
-        usersArrayList.add(new Users(tentaikhoan,email));
+        usersArrayList.add(new Users(tentaikhoan, email));
         ThongTinAdapter = new ThongTinAdapter(this, R.layout.navigation_thongtin, usersArrayList);
         listviewthongtin.setAdapter(ThongTinAdapter);
-
 
 
         //Chuyên mục
         navigationsArrayList = new ArrayList<>();
         navigationsArrayList.add(new ThongTin("Thông Tin", R.drawable.icon_login));
-        navigationsArrayList.add(new ThongTin("Đăng bài",R.drawable.icon_dangbai));
-        navigationsArrayList.add(new ThongTin("Ưa thích",R.drawable.baseline_favorite_red));
-        navigationsArrayList.add(new ThongTin("Thể Loại",R.drawable.icon_theloai));
-        navigationsArrayList.add(new ThongTin("Đổi mật khẩu",R.drawable.icon_doimatkhau));
-        navigationsArrayList.add(new ThongTin("Setting",R.drawable.icon_setting));
+        navigationsArrayList.add(new ThongTin("Đăng bài", R.drawable.icon_dangbai));
+        navigationsArrayList.add(new ThongTin("Ưa thích", R.drawable.baseline_favorite_red));
+        navigationsArrayList.add(new ThongTin("Thể Loại", R.drawable.icon_theloai));
+        navigationsArrayList.add(new ThongTin("Đổi mật khẩu", R.drawable.icon_doimatkhau));
+        navigationsArrayList.add(new ThongTin("Setting", R.drawable.icon_setting));
         navigationsArrayList.add(new ThongTin("Đăng Xuất", R.drawable.icon_logout));
 
-        NavigationAdapter = new NavigationAdapter(this,R.layout.layout_chuyenmuc,navigationsArrayList);
+        NavigationAdapter = new NavigationAdapter(this, R.layout.layout_chuyenmuc, navigationsArrayList);
         listviewmanhinhchinh.setAdapter(NavigationAdapter);
 
         //
-        btnYes = findViewById(R.id.btnYes);
-        btnNo = findViewById(R.id.btnNo);
+//        btnYes = findViewById(R.id.btnYes);
+//        btnNo = findViewById(R.id.btnNo);
 
     }
 
@@ -298,11 +296,11 @@ public class Home extends AppCompatActivity {
 
 
     // Hình ảnh tự chuyển động
-    private void autoImage(){
-        if(mListPhoto == null || mListPhoto.isEmpty() || viewPager == null) {
+    private void autoImage() {
+        if (mListPhoto == null || mListPhoto.isEmpty() || viewPager == null) {
             return;
         }
-        if(mTimer == null){
+        if (mTimer == null) {
             mTimer = new Timer();
         }
 
@@ -313,30 +311,31 @@ public class Home extends AppCompatActivity {
                     @Override
                     public void run() {
                         int currentItem = viewPager.getCurrentItem();
-                        int totalItem = mListPhoto.size()-1;
-                        if(currentItem<totalItem) {
+                        int totalItem = mListPhoto.size() - 1;
+                        if (currentItem < totalItem) {
                             currentItem++;
                             viewPager.setCurrentItem(currentItem);
-                        }
-                        else {
+                        } else {
                             viewPager.setCurrentItem(0);
                         }
                     }
                 });
             }
-        },500,3000);
+        }, 500, 3000);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mTimer != null) {
+        if (mTimer != null) {
             mTimer.cancel();
             mTimer = null;
         }
     }
+}
 
     //Xóa truyện
+
 //    private void DialogDelete(int i){
 //        Dialog dialog = new Dialog(this);
 //
--- app/src/main/java/edu/huflit/myapp/ThemTruyen.java
+++ app/src/main/java/edu/huflit/myapp/ThemTruyen.java
@@ -1,23 +1,15 @@
 package edu.huflit.myapp;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.UUID;
+import androidx.appcompat.app.AppCompatActivity;
 
 import edu.huflit.myapp.Model.TruyenTranh;
-import edu.huflit.myapp.Model.Users;
 import edu.huflit.myapp.database.dtbApp;
 
 public class ThemTruyen extends AppCompatActivity {
@@ -32,7 +24,7 @@ public class ThemTruyen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_truyen);
 
-        edtIDTruyen = findViewById(R.id.edtIDTruyen);
+
         edtNoiDung = findViewById(R.id.edtNoiDungTruyen);
         edtTieuDe = findViewById(R.id.edtTieuDe);
         edtIMG = findViewById(R.id.edtIMG);
@@ -75,7 +67,6 @@ public class ThemTruyen extends AppCompatActivity {
 
         TruyenTranh truyenTranh = new TruyenTranh();
 
-        truyenTranh.setIdTruyen(id);
         truyenTranh.setTenTruyen(tieuDe);
         truyenTranh.setLinkAnh(img);
         truyenTranh.setNoiDungTruyen(noiDung);
